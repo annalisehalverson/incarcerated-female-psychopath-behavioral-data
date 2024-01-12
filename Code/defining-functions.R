fd75a2dab70091ad02f600130d210ce65be2cf2c
annalisehalverson <asdh@uchicago.edu>
2024-01-12 13:45:46 -0600

    minor edits to function

--- Code/defining-functions.R
+++ Code/defining-functions.R
@@ -2,10 +2,10 @@
 
 
 # below is a fun lil conditional function with a while loop.
-# in this scenario, two locals in Hawaii are deciding how they want to get to school. they prefer to travel by ocean
+# in this scenario, two locals in Hawaii are deciding how they want to get to school. they prefer to travel by ocean.
 # if there are no waves, they will paddle.
 # if there are waves under 10 feet, they will surf.
-# if there are waves greater or equal to 10 feet, they will take the bus instead.
+# if there are waves greater or equal to 10 feet, it will be too dangerous so they will take the bus instead.
 hello_world <- function(waves) { # are there waves? T/F 
   if (waves == TRUE) { # if there are waves
     route <- "ho brah, we go surf" # surf to school
@@ -18,4 +18,4 @@ user_input <- as.numeric(readline("How big are the waves? Enter a number in feet
   paste0(route)
 }
 
-hello_world(TRUE)
+hello_world()
